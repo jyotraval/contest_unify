8aad83196f0a985e10276885dc41a469f1ff1637
Jyot Raval <ravaljyotg@gmail.com>
2025-06-23 23:01:28 +0530

    Update settings.py

--- ContestUnify/settings.py
+++ ContestUnify/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-#w^m&pej12ldg9m^5+*^*+q*^wpaot&9cvhmjr2p=zqv35ipz7'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#w^m&pej12ldg9m^5+*^*+q*^wpaot&9cvhmjr2p=zqv35ipz7'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
